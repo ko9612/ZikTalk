feda5fc3d7e8e27749dcd2fe8e5245103417cf3e
hanna_9 <kohana735@gmail.com>
2025-05-03 22:43:37 +0900

    style: layout 임의로 설정(#1)

--- client/src/components/common/Layout.jsx
+++ client/src/components/common/Layout.jsx
@@ -1,20 +1,27 @@
-// 해당 컴포넌트에 헤더 푸터
+// 해당 컴포넌트에 헤더 푸터, css임시로 대충 해둔거라 변경해야함
 import { Outlet, Link } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div>
-      <header className="bg-zik-main p-4 text-white">
-        <nav className="flex gap-4">
+    <div className="flex min-h-screen min-w-[1260px] flex-col">
+      {/* Header */}
+      <header className="bg-zik-main container-wrap h-[80px] text-white">
+        <div className="flex w-[1240px] gap-5 p-4 text-2xl">
           <Link to="/">홈</Link>
           <Link to="/interview">면접</Link>
-        </nav>
+        </div>
       </header>
-      <main className="p-8">
-        <Outlet />
+
+      {/* Main */}
+      <main className="container-wrap h-[100rem]">
+        <div className="w-[1240px] p-4">
+          <Outlet />
+        </div>
       </main>
-      <footer className="text-zik-text border-t p-4 text-center">
-        ⓒ 2025 ZikTalk
+
+      {/* Footer */}
+      <footer className="bg-zik-border text-zik-text container-wrap">
+        <div className="w-[1240px] p-4 text-[1.6rem]">ⓒ 2025 ZikTalk</div>
       </footer>
     </div>
   );
