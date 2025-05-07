3b47630064a9374cbb376cd61605b04b148ebee5
hanna_9 <kohana735@gmail.com>
2025-05-07 17:31:03 +0900

    fix: interview state 추가 (#35)

--- client/src/store/useVisibilityStore.jsx
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useVisibilityStore = create((set) => ({
-  topButtonVisible: false,
-  setTopButtonVisible: (visible) => set({ topButtonVisible: visible }),
-}));
-
-export default useVisibilityStore;
