fd74c0becf1cdc1f7d914e808cf8c7037bb7561a
hanna_9 <kohana735@gmail.com>
2025-05-08 20:20:41 +0900

    feat: 직무 store 추가 (#35)

--- client/src/components/common/Modal/CareerSelectModal.jsx
+++ client/src/components/common/Modal/CareerSelectModal.jsx
@@ -2,10 +2,12 @@ import Modal from "@/components/common/Modal/Modal";
 import React, { useState } from "react";
 import { careerData } from "@/data/carrerData";
 import Button from "@/components/common/Button";
+import { useRoleStore } from "@/store/store";
 
 const CareerSelectModal = ({ isOpen, onClose }) => {
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [selectedRole, setSelectedRole] = useState(null);
+  const setRoleValue = useRoleStore((state) => state.setRoleValue);
 
   const categrySelectHandler = (categ) => {
     if (selectedCategory !== categ) {
@@ -82,7 +84,7 @@ const CareerSelectModal = ({ isOpen, onClose }) => {
           shape="bar"
           color="violet"
           className="w-20 text-nowrap sm:w-28"
-          onClick={onClose}
+          onClick={() => setRoleValue(selectedRole)}
           disabled={!selectedRole}
         >
           선택
