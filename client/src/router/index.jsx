241df683d64c02dab9bf7dd24ac0a73c0a8df92b
hanna_9 <kohana735@gmail.com>
2025-05-11 02:09:38 +0900

    fix: landing loading 제외

--- client/src/router/index.jsx
+++ client/src/router/index.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
     children: [
       {
         path: "",
-        element: withSuspense(Landing),
+        element: <Landing />,
       },
       {
         path: "/signup",
