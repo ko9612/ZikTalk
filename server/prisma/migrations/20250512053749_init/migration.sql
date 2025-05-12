2b114ebaeb12228d5e1f6fd26031c155312ee566
lsohyuniil <lim07so10h@gmail.com>
2025-05-12 14:51:09 +0900

    prisma 스키마

---
+++ server/prisma/migrations/20250512053749_init/migration.sql
@@ -0,0 +1,58 @@
+-- CreateEnum
+CREATE TYPE "QuestionType" AS ENUM ('PERSONALITY', 'JOB');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "career" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Interview" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "role" TEXT NOT NULL,
+    "totalScore" INTEGER NOT NULL,
+    "summary" TEXT NOT NULL,
+    "strengths" TEXT NOT NULL,
+    "improvements" TEXT NOT NULL,
+    "personalityScore" INTEGER NOT NULL,
+    "personalityEval" TEXT NOT NULL,
+    "jobScore" INTEGER NOT NULL,
+    "jobEval" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "bookmarked" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Interview_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Question" (
+    "id" TEXT NOT NULL,
+    "interviewId" TEXT NOT NULL,
+    "order" INTEGER NOT NULL,
+    "type" "QuestionType" NOT NULL,
+    "content" TEXT NOT NULL,
+    "myAnswer" TEXT NOT NULL,
+    "videoUrl" TEXT,
+    "recommended" TEXT NOT NULL,
+    "bookmarked" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Question_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Interview" ADD CONSTRAINT "Interview_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Question" ADD CONSTRAINT "Question_interviewId_fkey" FOREIGN KEY ("interviewId") REFERENCES "Interview"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
