f4b8da9aab85749bb07e63edb302b2052accacf3
lsohyuniil <lim07so10h@gmail.com>
2025-05-18 10:20:07 +0900

    에러 발생 코드 삭제

--- client/src/pages/Landing/index.jsx
+++ client/src/pages/Landing/index.jsx
@@ -4,24 +4,9 @@ import FeatureSection from "@/pages/Landing/_components/FeatureSection";
 import GuideSection from "@/pages/Landing/_components/GuideSection";
 import HeroSection from "@/pages/Landing/_components/HeroSection";
 import KeyPointSection from "@/pages/Landing/_components/KeyPointSection";
-import React, { useEffect } from "react";
-import { fetchUserInfo } from "@/api/myPageApi";
+import React from "react";
 
 const index = () => {
-  useEffect(() => {
-    // 사용자 정보 확인 테스트
-    const checkUserInfo = async () => {
-      try {
-        const userInfo = await fetchUserInfo();
-        console.log("사용자 정보:", userInfo);
-      } catch (error) {
-        console.error("사용자 정보 가져오기 실패:", error);
-      }
-    };
-    
-    checkUserInfo();
-  }, []);
-
   return (
     <div>
       <HeroSection />
