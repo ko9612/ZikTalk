23405844da4d38e124b141402e623d13e014f1be
lsohyuniil <lim07so10h@gmail.com>
2025-05-21 10:46:31 +0900

    setTimeout, VITE_JWT_EXPIRY_TIME 삭제

--- client/src/api/signApi.js
+++ client/src/api/signApi.js
@@ -1,33 +1,44 @@
-// 로그인, 회원가입 관련 api
+/ 로그인, 회원가입 관련 api
+import { loginInfo } from "@/store/loginStore";
 import axiosInstance from "./axiosInstance";
-
-const serverUrl = import.meta.env.VITE_SERVER_URL;
 
 export const signin = async (data) => {
   try {
-    const res = await axiosInstance.post(`${serverUrl}/signin`, data);
+    const res = await axiosInstance.post("/signin", data);
+    const { userName } = res.data;
 
-    const { user, accessToken } = res.data;
+    onSigninSuccess(res);
 
-    // API 요청시 헤더에 accessToken 담아 보내도록 설정
-    axiosInstance.defaults.headers.common["Authorization"] =
-      `Bearer ${accessToken}`;
-
-    // 로컬 스토리지에 accessToken 저장 (MyInfo에서 활용)
-    localStorage.setItem("accessToken", accessToken);
-
-    console.log("[로그인] 토큰 설정 완료:", !!accessToken);
-
-    return { user };
+    return { userName };
   } catch (e) {
     console.error("로그인 중 오류 발생", e);
     throw e;
   }
 };
 
+export const onSilentRefresh = async () => {
+  try {
+    const res = await axiosInstance.post("/silent-refresh");
+
+    onSigninSuccess(res);
+  } catch (e) {
+    console.error("토큰 재발급 실패:", e);
+    const { logout } = loginInfo.getState();
+    logout();
+  }
+};
+
+const onSigninSuccess = (res) => {
+  const { accessToken } = res.data;
+
+  // accessToken을 axios 인스턴스 헤더에 설정
+  axiosInstance.defaults.headers.common["Authorization"] =
+    `Bearer ${accessToken}`;
+};
+
 export const signup = async (data) => {
   try {
-    await axiosInstance.post(`${serverUrl}/signup`, data);
+    await axiosInstance.post("/signup", data);
   } catch (e) {
     console.error("회원가입 중 오류 발생", e);
     throw e;
@@ -36,13 +47,35 @@ export const signup = async (data) => {
 
 export const verification = async (email) => {
   try {
-    const res = await axiosInstance.post(`${serverUrl}/verification`, {
+    const res = await axiosInstance.post("/verification", {
       email,
     });
 
     return res.data.verificationCode;
   } catch (e) {
     console.error("이메일 인증 실패", e);
+    throw e;
+  }
+};
+
+export const sendResetEmail = async (email) => {
+  try {
+    const res = await axiosInstance.post("/reset-password/request", { email });
+
+    return res.data;
+  } catch (e) {
+    console.error("비밀번호 재설정 이메일 발송 실패", e);
+    throw e;
+  }
+};
+
+export const resetPassword = async (data) => {
+  try {
+    const res = await axiosInstance.post("/reset-password/confirm", data);
+
+    return res.data;
+  } catch (e) {
+    console.error("비밀번호 재설정 실패", e);
     throw e;
   }
 };
