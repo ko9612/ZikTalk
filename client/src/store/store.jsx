f1810de433cbd6b7b8f555a7438d18cf11211948
hanna_9 <kohana735@gmail.com>
2025-05-26 22:16:45 +0900

    fix: 이전 코드로 변경

--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -1,69 +1,18 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-export const useVisibilityStore = create((set) => ({
-  topButtonVisible: false,
-  setTopButtonVisible: (visible) => set({ topButtonVisible: visible }),
-}));
-
-export const useInterviewTabStore = create((set) => ({
-  tabSelect: "설정",
-  setTabSelect: (str) => set({ tabSelect: str }),
-}));
-
-export const useSetupNavigationStore = create((set) => ({
-  currentComponent: "DeviceSetup",
-  navigateTo: (componentName) => set({ currentComponent: componentName }),
-  resetNavigation: () => set({ currentComponent: "DeviceSetup" }),
-}));
-
-export const useInterviewStateStore = create((set) => ({
-  interviewState: "question",
-  setInterviewState: (str) => set({ interviewState: str }),
-}));
-
-export const useLoadingStateStore = create((set) => ({
-  isLoading: true,
-  setIsLoading: (bool) => set({ isLoading: bool }),
-}));
-
-export const useReplyingStore = create((set) => ({
-  isReplying: false,
-  setIsReplying: (bool) => set({ isReplying: bool }),
-}));
-
-export const useRoleStore = create((set) => ({
-  roleValue: null,
-  setRoleValue: (str) => set({ roleValue: str }),
-}));
-
-export const useQuestionStore = create((set) => ({
-  interviewId: null,
-  curNum: 1,
-  questions: [],
-  answers: [],
-  video: [],
-  setInterviewId: (str) => set({ interviewId: str }),
-  setCurNum: (num) => set({ curNum: num }),
-  addQuestion: (str) =>
-    set((state) => ({
-      questions: [...state.questions, str],
-    })),
-
-  addAnswer: (str) =>
-    set((state) => ({
-      answers: [...state.answers, str],
-    })),
-
-  addVideo: (str) =>
-    set((state) => ({
-      video: [...state.video, str],
-    })),
-  resetInterview: () =>
-    set({
-      interviewid: null,
-      curNum: 1,
-      questions: [],
-      answers: [],
-      video: [],
+export const loginInfo = create(
+  persist(
+    (set) => ({
+      loginState: false,
+      userName: "",
+      setLoginState: (bool) => set({ loginState: bool }),
+      setUserName: (name) => set({ userName: name }),
+      logout: () => set({ loginState: false, userId: null, userName: "" }),
     }),
-}));
+    {
+      name: "login-info-storage",
+      getStorage: () => localStorage,
+    },
+  ),
+);
