31184b5d7ffb69027a2dba0b68b24e1b9c599ec8
JWH <44960019+hanjay3757@users.noreply.github.com>
2025-05-29 20:01:44 +0900

    문구위치/글꼴/버튼 통일

--- client/src/pages/myPage/_components/bookmark/EmptyBookmarkList.jsx
+++ client/src/pages/myPage/_components/bookmark/EmptyBookmarkList.jsx
@@ -2,31 +2,28 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 import { TableHeader, TEXT_COLORS } from "./settings";
-
+import Button from "@/components/common/Button";
 const EmptyBookmarkList = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="mx-auto w-full">
+    <div className="mx-auto w-full ">
       <TableHeader />
 
       <div
-        className={`flex flex-col items-center ${TEXT_COLORS.description} mt-10 min-h-[340px]`}
+        className={` flex min-h-[340px] flex-col items-center gap-2 sm:gap-5 mt-10 md:gap-5  ${TEXT_COLORS.normal}`}
       >
-        <div className="mb-5 flex h-20 w-20 items-center justify-center rounded-full bg-gray-100">
-          <FiSearch className={`text-5xl ${TEXT_COLORS.description}`} />
+        <div className="flex h-20 w-20 items-center justify-center rounded-full bg-gray-100">
+          <FiSearch className="text-5xl" />
         </div>
-        <span>북마크한 질문이 없습니다.</span>
-        <span className="mt-2 text-sm">
-          관심 있는 질문을 북마크하여 모아보세요.
-        </span>
-        <span className="text-sm">면접 준비에 도움이 됩니다.</span>
-        <button
-          className={`mt-4 rounded-md bg-indigo-500 px-4 py-2 ${TEXT_COLORS.button} transition-colors hover:bg-indigo-600`}
-          onClick={() => navigate("/interview")}
-        >
-          질문 찾아보기
-        </button>
+        <div className="flex flex-col items-center gap-1">
+          <p className="font-semibold">북마크한 질문이 없습니다</p>
+          <div className="text-center text-sm">
+            <p>관심 있는 질문을 북마크하여 모아보세요.</p>
+            <p>면접 준비에 도움이 됩니다.</p>
+          </div>
+        </div>
+        <Button onClick={() => navigate("/mypage/result-list")}>질문 북마크하기</Button>
       </div>
     </div>
   );
